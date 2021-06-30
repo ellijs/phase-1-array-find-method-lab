5e11d36fd9bc5de48cc1285e6acc422a33b5244f
ellijs <ellijs@hotmail.com>
2021-06-30 19:01:07 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
 
+function superbowlWin(array) {
+  let winningYear = array.find((array) => array.result === "W");
+  if (winningYear) {
+    return winningYear.year;
+  }
+}
+superbowlWin(record);
